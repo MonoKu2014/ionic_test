185c83c45a06daea0819246e55aff9bb9de2b9d5
Angelo Galaz Huerta <exagahu@dom_bci_prod.cl>
2019-05-24 17:49:27 -0400

    avances

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const TOKEN_KEY = 'auth-token';
 
@@ -11,9 +12,14 @@ const TOKEN_KEY = 'auth-token';
 export class AuthenticationService {
 
   authenticationState = new BehaviorSubject(false);
+  loginUrl = 'https://api-reservify.herokuapp.com/api/auth/login';
 
-  constructor(private storage: Storage, private plt: Platform) {
-    this.plt.ready().then(() => {
+  constructor(
+    private storage: Storage,
+    private platform: Platform,
+    public httpClient: HttpClient
+    ) {
+    this.platform.ready().then(() => {
       this.checkToken();
     });
   }
@@ -26,12 +32,20 @@ export class AuthenticationService {
     });
   }
 
-  login(email, password) {
-    console.log(email);
-    console.log(password);
-    return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
-      this.authenticationState.next(true);
+  login(emailUser, passwordUser) {
+    const body = {
+      email : emailUser,
+      password : passwordUser
+    };
+
+    return this.httpClient.post(this.loginUrl, body)
+    .toPromise()
+    .then(res => {
+      return res as any;
     });
+    /*return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
+      this.authenticationState.next(true);
+    });*/
   }
 
   logout() {
