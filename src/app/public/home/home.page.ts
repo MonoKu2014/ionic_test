e9dffb28c1b6acdae1503cc51e2321a902c25f4f
Angelo Galaz Huerta <exagahu@dom_bci_prod.cl>
2019-06-03 14:35:29 -0400

    avances home

--- src/app/public/home/home.page.ts
+++ src/app/public/home/home.page.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public goldServices: any;
+
+  constructor() {
+    this.goldServices = [];
+  }
+
+  ngOnInit() {
+    this.goldServices = [
+      {
+        enterprise: 'Bluemonkey',
+        sector: 'Desarrollo web',
+        description: 'Desarrollamos todo tipo de sotwares a medida para automatizar los procesos de tu empresa, ven y solicita tu hora'
+      },
+      {
+        enterprise: 'Bluemonkey',
+        sector: 'Desarrollo web',
+        description: 'Desarrollamos todo tipo de sotwares a medida para automatizar los procesos de tu empresa, ven y solicita tu hora'
+      },
+      {
+        enterprise: 'Bluemonkey',
+        sector: 'Desarrollo web',
+        description: 'Desarrollamos todo tipo de sotwares a medida para automatizar los procesos de tu empresa, ven y solicita tu hora'
+      },
+    ];
+  }
 
 }
